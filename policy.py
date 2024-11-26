4ec185ca253bef6d9bc18e7cdab41f232d955ad7
NGUYENG GIA TRONG <trongnguyengiatrong@gmail.com>
2024-11-26 18:25:36 +0700

    a

--- policy.py
+++ policy.py
@@ -9,7 +9,6 @@ mang7 = np.load("/home/giatrong/PycharmProjects/pythonProject/joint_angle_7.npy"
 mang8 = np.load("/home/giatrong/PycharmProjects/pythonProject/joint_angle_8.npy")
 np.set_printoptions(threshold=np.inf)
 print(mang1)
-
 print("mang3")
 print(mang3)
 
